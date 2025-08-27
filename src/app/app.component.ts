7dab96a3b6ce59dcde06953c43276e26412a8192
Matthieu-D <matthieu.debarge@cuma.fr>
2025-08-27 18:12:19 +0200

    exporter le poids

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { saveAs } from 'file-saver';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,7 @@ import { HttpClient } from '@angular/common/http';
 })
 export class AppComponent {
   url = 'https://weight-tracker-api-yj53.onrender.com'
+  //url = 'http://localhost:3000'
   title = 'weight-tracker';
   weight: number | null = null;
 
@@ -15,6 +17,34 @@ export class AppComponent {
   }
 
   onSubmit() {
-    this.http.post(this.url + '/weight', {poids: this.weight}).subscribe()
+    this.http.post(this.url + '/weight', {poids: this.weight}).subscribe(
+      () => alert("le poids a été saisi !")
+    )
+  }
+
+  exportAllWeights() {
+    this.http.get(this.url + '/weight').subscribe((data: any) => {
+      const csvData = this.convertToCSV(data);
+      const blob = new Blob([csvData], { type: 'text/csv;charset=utf-8;' });
+      saveAs(blob, 'weights.csv');
+    });
+  }
+
+  convertToCSV(objArray: any[]): string {
+    const array =
+      typeof objArray !== 'object' ? JSON.parse(objArray) : objArray;
+    let str = '';
+    const header = Object.keys(array[0]).join(',');
+    str += header + '\r\n';
+
+    for (const item of array) {
+      let line = '';
+      for (const index in item) {
+        if (line !== '') line += ',';
+        line += item[index];
+      }
+      str += line + '\r\n';
+    }
+    return str;
   }
 }
