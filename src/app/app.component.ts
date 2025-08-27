b45989d449d7a59015209f52c6f5b774170c0b81
Matthieu-D <matthieu.debarge@cuma.fr>
2025-08-27 15:54:49 +0200

    saisir un input et l'envoyer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  url = 'http://localhost:3000'
   title = 'weight-tracker';
+  weight: number | null = null;
+
+  constructor(private http: HttpClient) {
+  }
+
+  onSubmit() {
+    this.http.post(this.url + '/weight', {poids: this.weight}).subscribe()
+  }
 }
